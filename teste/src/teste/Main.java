052c6c8be2cc0542d856a5bf42203c3a00b9be27
kleitonalvesrb <kleiton.a.batista@gmail.com>
2016-08-01 17:45:30 -0300

    Update Main.java

--- teste/src/teste/Main.java
+++ teste/src/teste/Main.java
@@ -1,10 +1,11 @@
 package teste;
 
 public class Main {
-
+	// iniciando
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ois");
+		System.out.println("oi um");
+		System.out.println("oi dois");
 	}
 
 }
